55bae7a3a5c089c6427ff895c0b6f4320aee50a8
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-07-05 13:17:34 +0530

    Bean Lifecycle Methods

--- src/main/java/com/springdemo/practice/CricketCoach.java
+++ src/main/java/com/springdemo/practice/CricketCoach.java
@@ -1,11 +1,29 @@
 package com.springdemo.practice;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CricketCoach implements Coach{
+    public CricketCoach() {
+        System.out.println("In Constructor :"+getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes!!!!";
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff :"+getClass().getSimpleName());
+    }
+    @PreDestroy
+    public void doMyDestroyStuff(){
+        System.out.println("In doMyDestroyStuff :"+getClass().getSimpleName());
     }
 }
